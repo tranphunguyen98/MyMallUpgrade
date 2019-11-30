d18b95aac659471323735df4103c27abc284e971
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2019-11-30 12:08:53 +0700

    feat: Added HomeActivity with Navigation Drawer

---
+++ mobile-ui/src/main/java/com/example/mymallupgrade/ui/home/HomeActivity1.kt
@@ -0,0 +1,14 @@
+package com.example.mymallupgrade.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mymallupgrade.R
+
+class HomeActivity1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
--- mobile-ui/src/main/java/com/example/mymallupgrade/utils/ViewUtils.kt
+++ mobile-ui/src/main/java/com/example/mymallupgrade/utils/ViewUtils.kt
@@ -2,10 +2,10 @@ package com.example.mymallupgrade.utils
 
 import android.content.Context
 import android.content.Intent
-import com.example.mymallupgrade.ui.home.HomeActivity
+import com.example.mymallupgrade.ui.home.HomeActivity1
 
 fun Context.startHomeActivity() =
-    Intent(this,HomeActivity::class.java).also {
+    Intent(this,HomeActivity1::class.java).also {
         it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(it)
     }
