d73f268b0a90bc91ba702fa7573f7b87cebc65ac
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2019-12-23 11:17:26 +0700

    feat(presentation/FavoriteMovie): add ViewModel and State

---
+++ presentation/src/main/java/com/example/mymallupgrade/presentation/movie/FavoriteMovieState.kt
@@ -0,0 +1,22 @@
+package com.example.mymallupgrade.presentation.movie
+
+import com.example.mymallupgrade.presentation.entities.Movie
+
+/**
+ * Created by Tran Phu Nguyen on 12/23/2019.
+ */
+
+data class FavoriteMovieState(
+    val isLoading: Boolean = false,
+    val error: Throwable? = null,
+    val movies: List<Movie> = emptyList()
+) {
+    companion object {
+        fun idle(): FavoriteMovieState =
+            FavoriteMovieState(
+                isLoading = false,
+                movies = emptyList(),
+                error = null
+            )
+    }
+}
---
+++ presentation/src/main/java/com/example/mymallupgrade/presentation/movie/FavoriteMoviesViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.mymallupgrade.presentation.movie
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.mymallupgrade.domain.common.Mapper
+import com.example.mymallupgrade.domain.entity.movie.MovieEntity
+import com.example.mymallupgrade.domain.interactor.movie.GetFavoriteMovies
+import com.example.mymallupgrade.presentation.BaseViewModel
+import com.example.mymallupgrade.presentation.entities.Movie
+import timber.log.Timber
+
+/**
+ * Created by Tran Phu Nguyen on 12/12/2019.
+ */
+
+class FavoriteMoviesViewModel(
+    private val _getFavoriteMovies: GetFavoriteMovies,
+    private val mapper: Mapper<MovieEntity, Movie>
+) : BaseViewModel() {
+
+    private val _favoriteMoviesState = MutableLiveData<FavoriteMovieState>()
+    val favoriteMoviesState: LiveData<FavoriteMovieState>
+        get() = _favoriteMoviesState
+
+    init {
+        _favoriteMoviesState.value = FavoriteMovieState.idle()
+    }
+
+    fun getPopularMovies() {
+        _favoriteMoviesState.value = FavoriteMovieState(true)
+        addDispoable(_getFavoriteMovies()
+            .flatMap { mapper.observable(it) }
+            .subscribe({ movies ->
+                Timber.d("mv ${movies.size}")
+                FavoriteMovieState(false, null, movies)
+            }, {
+                Timber.d(it)
+                FavoriteMovieState(false, it)
+            })
+        )
+    }
+
+}
