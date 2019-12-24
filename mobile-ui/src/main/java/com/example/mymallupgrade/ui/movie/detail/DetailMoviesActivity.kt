bc8e01865ed6b04871bc47d07fc48e6d80745d27
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2019-12-24 12:07:44 +0700

    fix(DetailMovie): status bar not transparent

--- mobile-ui/src/main/java/com/example/mymallupgrade/ui/movie/detail/DetailMoviesActivity.kt
+++ mobile-ui/src/main/java/com/example/mymallupgrade/ui/movie/detail/DetailMoviesActivity.kt
@@ -47,8 +47,7 @@ class DetailMoviesActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_movies)
 
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE and View.SYSTEM_UI_FLAG_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE and View.SYSTEM_UI_FLAG_FULLSCREEN
 
         (application as App).createDetailComponent().inject(this)
 
