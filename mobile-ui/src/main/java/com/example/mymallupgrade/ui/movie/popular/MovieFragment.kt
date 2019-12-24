4543193c9d252500431fca37d5283d7079dbb0ed
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2019-12-24 18:30:33 +0700

    feat(MovieFragment): add scale animation for viewpager2

--- mobile-ui/src/main/java/com/example/mymallupgrade/ui/movie/popular/MovieFragment.kt
+++ mobile-ui/src/main/java/com/example/mymallupgrade/ui/movie/popular/MovieFragment.kt
@@ -78,22 +78,30 @@ class MovieFragment : Fragment() {
             clipChildren = false
             offscreenPageLimit = 3
             setPageTransformer { page, position ->
+
+                when {
+                    position < -1 ->
+                        page.scaleY = 0.8f
+                    position <= 1 -> {
+                        page.scaleY = Math.max(0.8f, 1 - Math.abs(position))
+                    }
+                    else -> {
+                        page.scaleY = 0.8f
+                    }
+                }
+
                 val viewPager = page.parent.parent as ViewPager2
                 val offset = position * - (2 * 30 + 30)
-                Timber.d("offset= ${offset}")
+
                 if(viewPager.orientation == ViewPager2.ORIENTATION_HORIZONTAL) {
                     if(ViewCompat.getLayoutDirection(viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
                         page.translationX = -offset
-                        Timber.d("offset= ${-offset}")
                     } else {
                         page.translationX = offset
-                        Timber.d("offset= + ${offset}")
 
                     }
                 } else {
                     page.translationX = offset
-                    Timber.d("offset= ? ${offset}")
-
                 }
             }
             adapter = popularSliderMoviesAdapter
