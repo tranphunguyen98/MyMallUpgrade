359bbdbf5bb4f4a4f5443800d9c644e2abdd69e5
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2019-12-25 16:34:21 +0700

    style: change style detail movie activity

--- mobile-ui/src/main/java/com/example/mymallupgrade/ui/movie/detail/DetailMoviesActivity.kt
+++ mobile-ui/src/main/java/com/example/mymallupgrade/ui/movie/detail/DetailMoviesActivity.kt
@@ -3,7 +3,9 @@ package com.example.mymallupgrade.ui.movie.detail
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.text.Layout.JUSTIFICATION_MODE_INTER_WORD
 import android.transition.Fade
 import android.transition.TransitionManager
 import android.view.View
@@ -18,8 +20,10 @@ import com.example.mymallupgrade.common.App
 import com.example.mymallupgrade.databinding.ActivityDetailMoviesBinding
 import com.example.mymallupgrade.presentation.movie.detail.DetailMovieViewModelFactory
 import com.example.mymallupgrade.presentation.movie.detail.MovieDetailViewModel
+import com.google.android.material.appbar.AppBarLayout
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail_movies.*
+import timber.log.Timber
 import javax.inject.Inject
 
 class DetailMoviesActivity : AppCompatActivity() {
@@ -47,7 +51,24 @@ class DetailMoviesActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_movies)
 
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE and View.SYSTEM_UI_FLAG_FULLSCREEN
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            binding.detailsOverviewSection.detailsOverview.justificationMode = JUSTIFICATION_MODE_INTER_WORD
+        }
+
+        setSupportActionBar(binding.toolbarMovieDetail)
+        supportActionBar?.apply {
+            title = ""
+            //setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding.detailsAppbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { p0, scrollRange ->
+            Timber.d(
+                "scollRagne= $scrollRange"
+            )
+        })
+
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE and View.SYSTEM_UI_FLAG_FULLSCREEN
 
         (application as App).createDetailComponent().inject(this)
 
@@ -75,7 +96,7 @@ class DetailMoviesActivity : AppCompatActivity() {
         handleObservable()
 
         binding.detailsBackButton.setOnClickListener {
-           onBackPressed()
+            onBackPressed()
         }
     }
 
