479167a1a5d62b692af727c1e055159faafb28bd
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-01-08 20:20:57 +0700

    fix(ui/DetailMovie): not async ui

--- mobile-ui/src/main/java/com/example/mymallupgrade/ui/movie/detail/DetailMoviesActivity.kt
+++ mobile-ui/src/main/java/com/example/mymallupgrade/ui/movie/detail/DetailMoviesActivity.kt
@@ -20,10 +20,8 @@ import com.example.mymallupgrade.common.App
 import com.example.mymallupgrade.databinding.ActivityDetailMoviesBinding
 import com.example.mymallupgrade.presentation.movie.detail.DetailMovieViewModelFactory
 import com.example.mymallupgrade.presentation.movie.detail.MovieDetailViewModel
-import com.google.android.material.appbar.AppBarLayout
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail_movies.*
-import timber.log.Timber
 import javax.inject.Inject
 
 class DetailMoviesActivity : AppCompatActivity() {
@@ -51,8 +49,9 @@ class DetailMoviesActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_movies)
 
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-            binding.detailsOverviewSection.detailsOverview.justificationMode = JUSTIFICATION_MODE_INTER_WORD
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            binding.detailsOverviewSection.detailsOverview.justificationMode =
+                JUSTIFICATION_MODE_INTER_WORD
         }
 
         setSupportActionBar(binding.toolbarMovieDetail)
@@ -61,11 +60,11 @@ class DetailMoviesActivity : AppCompatActivity() {
             //setDisplayHomeAsUpEnabled(true)
         }
 
-        binding.detailsAppbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { p0, scrollRange ->
-            Timber.d(
-                "scollRagne= $scrollRange"
-            )
-        })
+//        binding.detailsAppbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { p0, scrollRange ->
+//            Timber.d(
+//                "scollRagne= $scrollRange"
+//            )
+//        })
 
         window.decorView.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE and View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -125,13 +124,17 @@ class DetailMoviesActivity : AppCompatActivity() {
         transition.duration = 750
         TransitionManager.beginDelayedTransition(details_root_view, transition)
 
-        binding.detailsTitle.visibility = View.VISIBLE
-        binding.detailsReleaseDateLayout.visibility = View.VISIBLE
-        binding.detailsScoreLayout.visibility = View.VISIBLE
-        binding.detailsOverviewSection.root.visibility = View.VISIBLE
-        binding.detailsVideoSection.root.visibility = View.VISIBLE
-
-
+        binding.apply {
+            detailsTitle.visibility = View.VISIBLE
+            detailsReleaseDateLayout.visibility = View.VISIBLE
+            detailsScoreLayout.visibility = View.VISIBLE
+            detailsOverviewSection.root.visibility = View.VISIBLE
+            detailsVideoSection.root.visibility = View.VISIBLE
+            imgPlayDetail.visibility = View.VISIBLE
+            imgImdb.visibility = View.VISIBLE
+            detailsScore.visibility = View.VISIBLE
+            grChipType.visibility = View.VISIBLE
+        }
     }
 
     @SuppressLint("RestrictedApi")
